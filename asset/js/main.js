b6262f7fad74221a781300ba6793a10b21485e53
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-13 15:31:53 +0100

    to do list

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,7 +4,22 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            newTodo:'',
+            todos:[
+                { text: 'fare la spesa', done: false },
+                { text: 'studiare Vue.js', done: true },
+                { text: 'fare esercizi', done: false }
+            ]
             
         }
     }
-})
+    methods: {
+        addTodo() {
+            if (this.newTodo.trim() !== '') {
+                this.todos.push({ text: this.newTodo, done: false });
+                this.newTodo = '';
+            }
+        }
+    }
+
+}).mount('#app')
